f7d68302846cbed35f32f21f7e1d5daba56834f2
dannycastilloo <dannycastillootiniano@gmail.com>
2023-12-14 07:33:03 -0500

    fix filter

--- src/components/CountryCard/index.jsx
+++ src/components/CountryCard/index.jsx
@@ -1,34 +1,30 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { CountryContext } from '../../context';
 import useCountry from '../../hooks/useCountry';
 import { SkeletonCard } from '../SkeletonCard';
-import { useImage } from '../../hooks/useImage';
+import { useImage } from '../../hooks/useImage'
 
 import './index.css';
 
-export const CountryCard = ({ search }) => {
+export const CountryCard = ({ search, country }) => {
   const context = useContext(CountryContext);
-  const { loading, error, data: countryData } = useCountry()
-  const images = useImage()
-
-  const [imagesLoaded, setImagesLoaded] = useState(false);
+  const { loading, error, data: countryData } = useCountry();
+  const images = useImage();
 
   useEffect(() => {
-    // Move the state update to the useEffect
+    // Actualizar las imágenes en el contexto al cargar el componente
     context.setImages(images);
-    // Update local state when images are loaded
-    setImagesLoaded(true);
   }, [images, context]);
 
-  if (loading || error) return <SkeletonCard />
+  if (loading || error) return <SkeletonCard />;
 
   const handleCardClick = (country) => {
     context.setCountry({
       ...country,
       languages: country.languages,
       states: country.states
-    })
-  }
+    });
+  };
 
   const filteredCountries = countryData.countries.filter((item) => {
     const matchesSearch = search.toLowerCase() === '' || item.name.toLowerCase().includes(search);
