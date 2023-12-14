8edc9b4b9e0b7d03032f062c186f7b28e195edfa
dannycastilloo <dannycastillootiniano@gmail.com>
2023-12-14 01:59:57 -0500

    fix continent filtering

--- src/components/ContinentsModal/index.jsx
+++ src/components/ContinentsModal/index.jsx
@@ -1,51 +1,58 @@
-import { useContext } from 'react'
-import { CountryContext } from '../../context'
-import { Continent } from '../Continent'
-import './index.css'
+// ContinentsModal.js
+import { useContext } from 'react';
+import { CountryContext } from '../../context';
+import { Continent } from '../Continent';
+import './index.css';
 
 export const ContinentsModal = () => {
+  const context = useContext(CountryContext);
 
-    const context = useContext(CountryContext)
+  const handleContinentSelect = (continent) => {
+    context.setSelectedContinent(continent);
+    context.closeContinentsModal();
+  };
 
-    const handleContinentSelect = (continent) => {
-        context.setSelectedContinent(continent);
-        context.closeContinentsModal();
-    };
-
-    return (
-        <div className={`continents-modal
-             ${context.isContinentsModalOpen ? 'flex' : 'hidden'}`}>
-            <button>
-                <img
-                    src="./x.svg"
-                    alt="Cerrar"
-                    onClick={() => context.closeContinentsModal()} />
-            </button>
-            <div
-                className='continents-container'>
-
-                <Continent
-                    title='America'
-                    picture='./continents/america.jpg'
-                    onClick={() => handleContinentSelect('America')} />
-                <Continent
-                    title='Europe'
-                    picture='./continents/europa.jpg'
-                    onClick={() => handleContinentSelect('Europe')} />
-                <Continent
-                    title='Asia'
-                    picture='./continents/asia.jpg'
-                    onClick={() => handleContinentSelect('Asia')} />
-                <Continent
-                    title='Africa'
-                    picture='./continents/africa.jpg'
-                    onClick={() => handleContinentSelect('Africa')} />
-                <Continent
-                    title='Oceania'
-                    picture='./continents/oceania.jpg'
-                    onClick={() => handleContinentSelect('Oceania')} />
-            </div>
-        </div>
-
-    )
-}
+  return (
+    <div className={`continents-modal ${context.isContinentsModalOpen ? 'flex' : 'hidden'}`}>
+      <button>
+        <img
+          src="./x.svg"
+          alt="Cerrar"
+          onClick={() => context.closeContinentsModal()}
+        />
+      </button>
+      <div className='continents-container'>
+        <Continent
+          title='South America'
+          picture='./continents/south-america.jpg'
+          onClick={() => handleContinentSelect('South America')}
+        />
+        <Continent
+          title='North America'
+          picture='./continents/north-america.jpg'
+          onClick={() => handleContinentSelect('North America')}
+        />
+        <Continent
+          title='Europe'
+          picture='./continents/europa.jpg'
+          onClick={() => handleContinentSelect('Europe')}
+        />
+        <Continent
+          title='Asia'
+          picture='./continents/asia.jpg'
+          onClick={() => handleContinentSelect('Asia')}
+        />
+        <Continent
+          title='Africa'
+          picture='./continents/africa.jpg'
+          onClick={() => handleContinentSelect('Africa')}
+        />
+        <Continent
+          title='Oceania'
+          picture='./continents/oceania.jpg'
+          onClick={() => handleContinentSelect('Oceania')}
+        />
+      </div>
+    </div>
+  );
+};
